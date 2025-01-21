d585a47636f8795029a8b1fbf218f27dfabb29e2
ahyoung <jr3537@naver.com>
2025-01-21 15:49:06 +0900

    test

---
+++ src/main/java/com/example/refactoredproject/RefactoredProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.refactoredproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoredProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoredProjectApplication.class, args);
+	}
+
+}
